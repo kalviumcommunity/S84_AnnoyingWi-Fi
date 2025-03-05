2c5a517aaa64414cf7d598853c284c0fd5c5b857
harshitsharmas84 <harshit.sharma.s84@kalvium.community>
2025-03-05 12:22:48 +0530

    starting

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const routes = require("./routes/routes");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api", routes);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
